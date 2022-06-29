c9f1955c4d5ee51660638c71c2f19b6e0546cd4f
SE150329 <nguyennhse150329@fpt.edu.vn>
2022-06-29 14:09:43 +0700

    Fix

--- BusinessObject/MemberObject.cs
+++ BusinessObject/MemberObject.cs
@@ -8,5 +8,25 @@
         public string Password { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+
+        public MemberObject()
+        {
+        }
+
+        public MemberObject(int memberID, string memberName, string email, string password, string city, string country)
+        {
+            MemberID = memberID;
+            MemberName = memberName;
+            Email = email;
+            Password = password;
+            City = city;
+            Country = country;
+        }
+
+        public override string? ToString()
+        {
+            return $"[Member ID: {MemberID}]" + $"[Member name: {MemberName}]" + $"[Email: {Email}]" + $"[City: {City}]" + $"[Country :{Country}]";
+        }
     }
+
 }
